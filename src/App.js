ee4f309a6a1f7bc3130cff9197c6e2bcc88b270c
Tuozzo <lucas.javier.tuozzo@accenture.com>
2019-06-01 16:44:06 -0300

    Added API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { fetchTopRated, fetchPopular, fetchUpcoming } from './moviesService'
 
 function App() {
   return (
---
+++ src/movieService.js
@@ -0,0 +1,23 @@
+/*Traer próximamente*/
+
+export const fetchUpcoming = () => fetch(
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=6f26fd536dd6192ec8a57e94141f8b20"
+)
+  .then(res => res.json())
+  .then((json) => json.results)
+
+/*Traer mejores rankeados*/
+
+export const fetchTopRated = () => fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=6f26fd536dd6192ec8a57e94141f8b20"
+)
+  .then(res => res.json())
+  .then((json) => json.results)
+
+/*Traer populares*/
+
+export const fetchPopular = () => fetch(
+  "https://api.themoviedb.org/3/movie/popular?api_key=6f26fd536dd6192ec8a57e94141f8b20"
+)
+  .then(res => res.json())
+  .then((json) => json.results)
