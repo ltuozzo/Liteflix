c223d78d21e1fff86c2688b986195e7252b2f22d
Tuozzo <lucas.javier.tuozzo@accenture.com>
2019-06-03 00:01:36 -0300

    Nav bar and footer added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import './App.css';
-import data from './data/data';
-import MovieCard from './movieCard';
-import listaPeliculas from './moviesService'
+import { ReactComponent as Logo } from './liteflix.svg'
+import { fetchTopRated, fetchPopular, fetchUpcoming } from './moviesService'
+
+class NavBar extends React.Component {
+  render() {
+    return <nav>
+      <Logo />
+      <a href="index.html">Inicio</a>
+      <a href="index.html">Series</a>
+      <a href="index.html">Películas</a>
+      <a href="index.html">Agregados recientemente</a>
+      <a href="index.html">Mi lista</a>
+    </nav>
+  }
+}
+
+class Hero extends React.Component {
+  render() {
+    return <div id="portada">
+      <h2>ORIGINAL DE <b>LITEFLIX</b></h2>
+      <h1>Kids at school</h1>
+      <br/><br/><br/><br/>
+      <p><strong>Ver temporada 1</strong></p>
+      <p>Lorem ipsum dolor amet meh freegan snackwave kombucha gastropub. Neutra cray street art freegan hoodie drinking vinegar. Before they sold out copper mug taiyaki knausgaard deep v flannel post-ironic lumbersexual selfies chillwave fanny pack tbh hammock.</p>
+    </div>
+  }
+}
+
+/*Estructura de una película*/
+const Movie = ({ movie }) => {
+  return <div class="tile">
+    <div class="tile__media">
+      <img class="tile__img" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}/>
+    </div>
+    <div class="tile__details">
+      <div class="tile__title">
+        {movie.title}
+      </div>
+      <div class="tile__genre">
+        {movie.vote_average}
+      </div>
+    </div>
+  </div>
+};
+
+/*Estructura de películas populares*/
+const PopularMovie = ({ movie }) => {
+  return <div class="tile">
+    <div class="tile__media">
+      <img class="tile__img__poster" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+    </div>
+  </div>
+};
 
 class UpcomingMovies extends React.Component {
   constructor(props) {
@@ -11,12 +61,11 @@ class UpcomingMovies extends React.Component {
     this.state = { movies: [] };
   }
 
-  /*Crear nuevo array con las películas correspondientes a esta row*/
   componentDidMount() {
-    data.movieProperties.map( movies => movies.push((data.movieProperties,i) => (data.movieProperties[i].proximamente) ? data.movieProperties));
+    fetchUpcoming()
+      .then(movies => this.setState({ movies }));
   }
 
-  /*Crear cada película individual*/
   render() {
     return React.createElement(
       "div",
@@ -33,12 +82,11 @@ class TopRatedMovies extends React.Component {
     this.state = { movies: [] };
   }
 
-  /*Crear nuevo array con las películas correspondientes a esta row*/
   componentDidMount() {
-    data.movieProperties.map( movies => movies.push(data.movieProperties => (data.movieProperties.mejorRankeada) ? data.movieProperties));
+    fetchTopRated()
+      .then(movies => this.setState({ movies }));
   }
 
-  /*Crear cada película individual*/
   render() {
     return React.createElement(
       "div",
@@ -55,12 +103,11 @@ class PopularMovies extends React.Component {
     this.state = { movies: [] };
   }
 
-  /*Crear nuevo array con las películas correspondientes a esta row*/
   componentDidMount() {
-    data.movieProperties.map( movies => movies.push(data.movieProperties => (data.movieProperties.popular) ? data.movieProperties));
+    fetchPopular()
+      .then(movies => this.setState({ movies }));
   }
 
-  /*Crear cada película individual*/
   render() {
     return React.createElement(
       "div",
@@ -70,65 +117,42 @@ class PopularMovies extends React.Component {
   }
 }
 
-class MovieSlider extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      movieProperties: data.movieProperties,
-      movieProp: data.movieProperties[0]
-    }
-  }
-
-  /*Botónes para mover la fila*/
-  nextMovie = () => {
-    const newIndex = this.state.movieProp.index+1;
-    this.setState({
-      movieProp: data.movieProperties[newIndex]
-    })
-  }
-
-  prevMovie = () => {
-    const newIndex = this.state.movieProp.index-1;
-    this.setState({
-      movieProp: data.movieProperties[newIndex]
-    })
-  }
-
-  /*Crear fila de películas*/
+class Footer extends React.Component {
   render() {
-    const {movieProperties, movieProp} = this.state;
-    return (
-      <div>
-        <button
-          onClick={() => this.nextMovie()}
-          disabled={movieProp.index === data.movieProperties.length-1}
-        >Next</button>
-        <button
-          onClick={() => this.prevMovie()}
-          disabled={movieProp.index === 0}
-        >Prev</button>
-        <div className="col">
-          <div className={`cards-slider`}>
-            <div className="cards-slider-wrapper" style={{
-              'transform': `translateX(-${movieProp.index*(100/movieProperties.length)}%)`
-            }}>
-            {
-              movieProperties.map(movieProp => <MovieCard key={movieProp._id} movieProp={movieProp} />)
-            }
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <div class="footer">
+      <table>
+        <tr>
+          <td>Todos los títulos</td>
+          <td>Cancelar subscripción</td>
+          <td>Ayuda</td>
+        </tr>
+        <tr>
+          <td>Prensa</td>
+          <td>Inversores</td>
+          <td>RRHH</td>
+        </tr>
+        <tr>
+          <td>Confidencialidad</td>
+          <td>Legales</td>
+          <td>Cookies</td>
+        </tr>
+        <tr>
+          <td>Contacto</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+      <p>© 1997-2018 Liteflix</p>
+    </div>
   }
 }
 
-/*App*/
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Hero />
       <div class="row">
-        <listaPeliculas />
         <h3>Próximamente</h3>
         <div class="row__inner">
           <UpcomingMovies />
@@ -141,7 +165,12 @@ function App() {
         <div class="row__inner">
           <PopularMovies />
         </div>
+        <h3>Drama</h3>
+        <div class="row__inner">
+          <UpcomingMovies />
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- src/moviesService.js
+++ src/moviesService.js
@@ -1,34 +1,31 @@
-import data from './data/data';
-
 /*Traer próximamente*/
-const fetchUpcoming = () => fetch(
+
+export const fetchUpcoming = () => fetch(
   "https://api.themoviedb.org/3/movie/upcoming?api_key=6f26fd536dd6192ec8a57e94141f8b20"
 )
   .then(res => res.json())
   .then((json) => json.results)
 
 /*Traer mejores rankeados*/
-const fetchTopRated = () => fetch(
+
+export const fetchTopRated = () => fetch(
   "https://api.themoviedb.org/3/movie/top_rated?api_key=6f26fd536dd6192ec8a57e94141f8b20"
 )
   .then(res => res.json())
   .then((json) => json.results)
 
 /*Traer populares*/
-const fetchPopular = () => fetch(
+
+export const fetchPopular = () => fetch(
   "https://api.themoviedb.org/3/movie/popular?api_key=6f26fd536dd6192ec8a57e94141f8b20"
 )
   .then(res => res.json())
   .then((json) => json.results)
 
 /*Traer generos*/
-const fetchGenres = () => fetch(
+
+export const fetchGenres = () => fetch(
   "https://api.themoviedb.org/3/genre/movie/list?api_key=6f26fd536dd6192ec8a57e94141f8b20"
 )
   .then(res => res.json())
   .then((json) => json.results)
-
-/*Concatenar las películas locales y de la API en un json*/
-export listaPeliculas() {
-  
-}
